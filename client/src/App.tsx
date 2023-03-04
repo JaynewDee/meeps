d364539bbef7522da74571d0338972bf1250b152
NuminousBlue <jdiehl2236@gmail.com>
2023-03-04 00:53:35 -0600

    troubleshooting myself in the foot

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -17,7 +17,7 @@ function App() {
   useEffect(() => {
     //
 
-    const socket = io("//localhost:3001"); // connect to the current host and port
+    const socket = io(); // connect to the current host and port
 
     //
 
