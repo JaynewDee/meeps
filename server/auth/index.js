2866c3b79953e4548ff827364c2c910851ded188
NuminousBlue <jdiehl2236@gmail.com>
2023-03-05 01:10:26 -0600

    full-stack auth

---
+++ server/auth/index.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+
+const secret = process.env.JWT_SECRET;
+
+const jwtAuth = {
+  middleware: (req, res, next) => {
+    const { token } = req.body;
+
+    console.log(`TOKEN: ${token}`);
+
+    if (!token) return res.json({ message: "NOT AUTHORIZED" });
+
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: "6h" });
+
+      if (data) next();
+      else return res.json({ message: "NOT AUTHORIZED" });
+    } catch (err) {
+      console.error(err);
+    }
+  },
+  sign: (userCreds) =>
+    jwt.sign({ data: userCreds }, secret, { expiresIn: "6h" })
+};
+
+module.exports = jwtAuth;
--- server/routing/routes.js
+++ server/routing/routes.js
@@ -1,18 +1,20 @@
 const User = require("../models/User");
+const jwtAuth = require("../auth/index");
 
 const htmlRouter = require("express").Router();
 
-htmlRouter.get("*", (req, res, next) => {
+htmlRouter.get("*", (_, res) => {
   res.sendFile(join(__dirname, "../client/index.html"));
 });
 
 const apiRouter = require("express").Router();
 
-apiRouter.post("/user", async (req, res, next) => {
+apiRouter.post("/user/new", async (req, res, next) => {
   const { body } = req;
   console.log(body);
   res.status(200).json({ message: "Hello!" });
-  //   const user = await User.create(body);
+  const user = await User.create(body);
+  const token = jwtAuth.sign(body);
 });
 
 module.exports = { htmlRouter, apiRouter };
