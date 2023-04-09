6984035d01ac58eeff69466078fadd1c13c26571
NuminousBlue <jdiehl2236@gmail.com>
2023-04-08 22:03:24 -0500

    readme

---
+++ README.md
@@ -0,0 +1,23 @@
+# _ROOMY_
+
+## **Join a room and speak your small piece**
+
+---
+
+### Roomy is inspired by early-internet chat rooms.
+
+---
+
+---
+
+---
+
+> > > #### Mobile view
+> > >
+> > > ![Mobile View](./assets/readme/mobile-view.png)
+
+---
+
+> > > #### Desktop preview
+> > >
+> > > ![Desktop/Large View](./assets/readme/desktop-view.png)
--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,13 +1,29 @@
-import { BsGear as Gear, BsQuestionCircle as Question } from "react-icons/bs";
+import {
+  BsGear as Gear,
+  BsQuestionCircle as Question,
+  BsDoorOpen as ExitDoor
+} from "react-icons/bs";
+import { AuthHandle } from "../auth/auth";
+import { useUserContext } from "../utils/context";
 
 const Header = () => {
+  const { logout } = useUserContext();
+
+  const destroySession = () => {
+    AuthHandle.logout();
+    logout();
+  };
+
   return (
     <header>
       <h1 className="app-title">ROOMY</h1>
       <div className="menu-options">
         {/* On click, display settings modal */}
-        <span className="settings-icon">{Gear({})}</span>
-        <span className="help-icon">{Question({})}</span>
+        <span className="settings-icon menu-icon">{Gear({})}</span>
+        <span className="help-icon menu-icon">{Question({})}</span>
+        <span className="exit-icon menu-icon" onClick={destroySession}>
+          {ExitDoor({})}
+        </span>
       </div>
     </header>
   );
