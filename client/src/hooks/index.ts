43dccc5eded180763feb0af341672ab6edd77801
NuminousBlue <jdiehl2236@gmail.com>
2023-04-16 19:40:39 -0500

    extract useUserSettings hook from index

--- client/src/hooks/index.ts
+++ client/src/hooks/index.ts
@@ -1,42 +1,11 @@
-import { useEffect, useState } from "react";
-import { LSItemHandler } from "../storage";
 import { useChatSocket, type SocketProp } from "./socket";
-import { useThemeSettings } from "./styles";
 import {
   useAuthValidation,
   useMessageValidation,
   handleError
 } from "./validation";
-
-export type Settings = {
-  displayName: string;
-  hideRealName: boolean;
-  currentRoom: string;
-  currentTheme: string;
-};
-
-const useUserSettings = () => {
-  const currentStore = new LSItemHandler("settings");
-  const hasCurrentStore = currentStore.exists();
-
-  const loadState = hasCurrentStore
-    ? currentStore.get()
-    : {
-        displayName: "",
-        hideRealName: true,
-        currentRoom: "central",
-        currentTheme: "Mono Ocean"
-      };
-
-  const [userSettings, setUserSettings] = useState<any>(loadState);
-
-  useEffect(() => {
-    const storage = new LSItemHandler("settings");
-    storage.set(userSettings);
-  }, [userSettings]);
-
-  return [userSettings, setUserSettings];
-};
+import { useUserSettings } from "./settings";
+import { useThemeSettings } from "./styles";
 
 export {
   useChatSocket,
